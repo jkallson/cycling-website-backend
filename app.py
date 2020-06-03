ba30656d5d3e4ece68d2248eb145c8d3e41bd272
jkallson <janar.kallson@gmail.com>
2020-06-03 20:15:29 +0300

    Added a way to get all rides.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import config
 from flask import Flask
 from firebase_admin import credentials
 from firebase_admin import firestore
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -14,17 +15,19 @@ db = firestore.client()
 
 
 
-@app.route('/')
-def hello_world():
-    users_ref = db.collection(u'rides')
-    docs = users_ref.stream()
+@app.route('/rides',methods=['GET'])
+def get_rides():
+    #Getting all documents and ordering them by day
+    query = db.collection("rides").order_by("day")
+    docs = query.stream()
+    result = []
 
+    #Adding all documents to a list
     for doc in docs:
-        print(u'{} => {}'.format(doc.id, doc.to_dict()))
+        result.append(doc.to_dict())
 
-
-    return "Request completed"
-
+    #Creating json out of that list and returning it
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run()
